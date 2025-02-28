859d9f8e4cd65f7c6ef63d835394fb69379e3d1f
Pulpyyyy <32004297+Pulpyyyy@users.noreply.github.com>
2025-02-28 15:54:37 +0100

    Remove availability in discovery device (otherwise never created)

--- src/carconnectivity_plugins/mqtt_homeassistant/plugin.py
+++ src/carconnectivity_plugins/mqtt_homeassistant/plugin.py
@@ -104,9 +104,6 @@ class Plugin(BasePlugin):  # pylint: disable=too-many-instance-attributes
             'device': {
                 'ids': vin,
                 'sn': vin,
-                'availability_topic': f'{self.mqtt_plugin.mqtt_client.prefix}/plugins/{self.mqtt_plugin.mqtt_client.plugin_id}/connected',
-                'payload_not_available': 'False',
-                'payload_available': 'True',
             },
             'origin': {
                 'name': 'carconnectivity-plugin-mqtt',
